1e36a03c2dee7ec712565541d7c8bad6be1bf644
Christian Schmelz <christian.schmelz@protonmail.com>
2024-01-17 16:05:08 +0200

    Added mock data + improved README

--- README.md
+++ README.md
@@ -2,17 +2,19 @@
 
 Demo app showcasing FHE Sentiment analysis using ZAMA's sentiment analysis demo.
 
+
 ## Getting Started
 
 Clone the repo.
 
 +++ Add steps to run the app
 
+
 ## How Fully Homomorphic Encryption is performed to guarantee no trust collaboration
 
 **Loading the text file** - A text file is loaded from the user's device that contains the message that needs to analysed.
 
-**Reading the file** - The contents of the file are parsed.
+**Reading the file** - The contents of the file are parsed on device.
 
 **Encrypting the file content** - Using RSA encryption, the content encrypted using the public key and the private key is securely stored on device.
 
@@ -23,6 +25,7 @@ Clone the repo.
 **Receiving the encrypted server response** - Having the public key already stored, the server only responds with the results of the analysis.
 
 **Decrypting the response** - Using the private key, the server's response is decrypted and shown to the user.
+
 
 ## No trust collaboration
 
@@ -37,3 +40,20 @@ Clone the repo.
 **The server can't decrypt the message** - The only way to decrypt the message is to use the private key, which the server does not know.
 
 **The data is only visible to the user** - Because of the steps described above, the data cannot be read by anybody else who does not have access to the user's device.
+
+## Lack of End-to-End encryption
+
+**Automatic Cloud Backup** - On mobile phones most users have automatic cloud backups enabled by default. This means that the contents on the device are automatically loaded on Apple's or Google's cloud storage.
+
+**US Laws force cloud service providers to hand over data** - Due to certain laws in the US (maybe other states as well) cloud service providers are forced to handle user data in unsecure ways. I.e. they are forced to hand data over to law enforcement.
+
+**Private key storage** - Because of this, no data can ever be truly encrypted on any mobile smartphone as long as automatic cloud backups are enabled.
+
+
+## Libraries used
+
+**ZAMA AI FHE Sentiment Analysis Demo** - [encrypted_sentiment_analysis](https://huggingface.co/spaces/zama-fhe/encrypted_sentiment_analysis/tree/main)
+
+**RSA Encryption** - [Pointy Castle - Dart Derived Bouncy Castle APIs](https://github.com/bcgit/pc-dart)
+
+**File picker** - [File picker plugin for Flutter](https://github.com/miguelpruivo/flutter_file_picker)
--- lib/services/crypto.service.dart
+++ lib/services/crypto.service.dart
@@ -10,30 +10,24 @@ class CryptoService {
   factory CryptoService() => _instance;
 
   Uint8List rsaEncrypt(RSAPublicKey myPublic, Uint8List dataToEncrypt) {
-    final encryptor = OAEPEncoding(RSAEngine())
-      ..init(true, PublicKeyParameter<RSAPublicKey>(myPublic)); // true=encrypt
+    final encryptor = OAEPEncoding(RSAEngine())..init(true, PublicKeyParameter<RSAPublicKey>(myPublic)); // true=encrypt
 
     return _processInBlocks(encryptor, dataToEncrypt);
   }
 
   Uint8List rsaDecrypt(RSAPrivateKey myPrivate, Uint8List cipherText) {
-    final decryptor = OAEPEncoding(RSAEngine())
-      ..init(false, PrivateKeyParameter<RSAPrivateKey>(myPrivate)); // false=decrypt
+    final decryptor = OAEPEncoding(RSAEngine())..init(false, PrivateKeyParameter<RSAPrivateKey>(myPrivate)); // false=decrypt
 
     return _processInBlocks(decryptor, cipherText);
   }
 
-  AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateRSAkeyPair(
-      SecureRandom secureRandom,
-      {int bitLength = 2048}) {
+  AsymmetricKeyPair<RSAPublicKey, RSAPrivateKey> generateRSAkeyPair(SecureRandom secureRandom, {int bitLength = 2048}) {
     // Create an RSA key generator and initialize it
 
     // final keyGen = KeyGenerator('RSA'); // Get using registry
     final keyGen = RSAKeyGenerator();
 
-    keyGen.init(ParametersWithRandom(
-        RSAKeyGeneratorParameters(BigInt.parse('65537'), bitLength, 64),
-        secureRandom));
+    keyGen.init(ParametersWithRandom(RSAKeyGeneratorParameters(BigInt.parse('65537'), bitLength, 64), secureRandom));
 
     // Use the generator
 
@@ -48,34 +42,25 @@ class CryptoService {
   }
 
   SecureRandom getSecureRandom() {
-
-    final secureRandom = SecureRandom('Fortuna')
-      ..seed(KeyParameter(
-          Platform.instance.platformEntropySource().getBytes(32)));
+    final secureRandom = SecureRandom('Fortuna')..seed(KeyParameter(Platform.instance.platformEntropySource().getBytes(32)));
     return secureRandom;
   }
 
   Uint8List _processInBlocks(AsymmetricBlockCipher engine, Uint8List input) {
-    final numBlocks = input.length ~/ engine.inputBlockSize +
-        ((input.length % engine.inputBlockSize != 0) ? 1 : 0);
+    final numBlocks = input.length ~/ engine.inputBlockSize + ((input.length % engine.inputBlockSize != 0) ? 1 : 0);
 
     final output = Uint8List(numBlocks * engine.outputBlockSize);
 
     var inputOffset = 0;
     var outputOffset = 0;
     while (inputOffset < input.length) {
-      final chunkSize = (inputOffset + engine.inputBlockSize <= input.length)
-          ? engine.inputBlockSize
-          : input.length - inputOffset;
+      final chunkSize = (inputOffset + engine.inputBlockSize <= input.length) ? engine.inputBlockSize : input.length - inputOffset;
 
-      outputOffset += engine.processBlock(
-          input, inputOffset, chunkSize, output, outputOffset);
+      outputOffset += engine.processBlock(input, inputOffset, chunkSize, output, outputOffset);
 
       inputOffset += chunkSize;
     }
 
-    return (output.length == outputOffset)
-        ? output
-        : output.sublist(0, outputOffset);
+    return (output.length == outputOffset) ? output : output.sublist(0, outputOffset);
   }
 }
